5c990a3312b94ad181fa710478b83b232213e29c
krubaisamessi <kruba3852@gmail.com>
2026-06-10 00:04:59 +0530

    Create alu.v

---
+++ alu.v
@@ -0,0 +1,33 @@
+//==================================================
+// Low Power ALU Design Using VLSI
+// Operations:
+// 00 - ADD
+// 01 - SUB
+// 10 - AND
+// 11 - OR
+//==================================================
+
+module ALU (
+    input enable,
+    input [3:0] A,
+    input [3:0] B,
+    input [1:0] Sel,
+    output reg [3:0] Result
+);
+
+always @(*) begin
+    if (enable) begin
+        case (Sel)
+            2'b00: Result = A + B;   // Addition
+            2'b01: Result = A - B;   // Subtraction
+            2'b10: Result = A & B;   // AND
+            2'b11: Result = A | B;   // OR
+            default: Result = 4'b0000;
+        endcase
+    end
+    else begin
+        Result = 4'b0000;
+    end
+end
+
+endmodule
